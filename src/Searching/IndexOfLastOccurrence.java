175dc281ea482be399a8f8bbd24bc5c8c04634df
chaitra <chaitracs2000@gmail.com>
2023-09-24 13:03:14 +0530

    BS: Index of Occurrences

---
+++ src/Searching/IndexOfLastOccurrence.java
@@ -0,0 +1,37 @@
+package Searching;
+
+/*
+Index of Last Occurrence in Sorted
+
+Iterative : https://ide.geeksforgeeks.org/cx1jW3Apo9
+TC: O(log n)
+SC: O(1)
+ */
+public class IndexOfLastOccurrence {
+
+    public static int getFirstIndex(int[] arr, int x) {
+        int n = arr.length;
+        int low = 0, high = n-1, mid;
+        while(low <= high) {
+            mid = (low+high)/2;
+            if (arr[mid] < x)
+                low = mid+1;
+            else if (arr[mid] > x)
+                high = mid-1;
+            else {
+                if(mid == high || arr[mid] != arr[mid+1])
+                    return mid;
+                else
+                    low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 10, 10, 10, 20, 20, 40};
+        int target = 20;
+        System.out.println("Index of last occurrence of target " + getFirstIndex(arr, target));
+    }
+
+}
